25d76b52602a3eab4ef119b07b76f87c93317220
Dennis McQuilken <dennismcquilken@laptop73-ma-us.local>
2018-06-11 12:10:29 -0400

    refactoring and added controller

--- app.js
+++ app.js
@@ -4,8 +4,14 @@ const express = require('express');
 
 const app = express();
 
+//initialize env
+if(!process.env.NODE_ENV) {
+	console.log('No environment defined.  Defaulting to "development"');
+	process.env.NODE_ENV == 'development';
+}
+
 //setup logging
-app.use(logger('development'));
+app.use(logger(process.env.NODE_ENV));
 
 //init server
 const server = require('./server/server')(app);
